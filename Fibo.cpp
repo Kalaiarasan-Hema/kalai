e5dd8b764d7b1167d1752c71227792455b01e3c3
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-03-19 09:28:47 +0530

    Create Fibo.cpp

---
+++ Fibo.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+class Fibo
+{
+int n,a=0,b=1,s=1,i;
+void get()
+{
+    cout<<"INPUT"<<endl;
+    cin>>n;
+}
+void fibo()
+{
+    cout<<"OUTPUT"<<endl;
+    for(i=0;i<n;i++)
+    {
+        cout<<s;
+        s=a+b;
+        a=b;
+        b=s;
+    }
+}
+public:
+Fibo()
+{
+    get();
+    fibo();
+}
+};
+int main()
+{
+Fibo f;
+return 0;
+}
