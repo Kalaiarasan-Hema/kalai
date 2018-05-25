219bf7aac4591ad75baf67775c2ebc0f55a46111
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 11:47:08 +0530

    Create Begin107.cpp

---
+++ Begin107.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+class Begin107
+{
+private:
+public:
+Begin107()
+{
+int a,b,c;
+cin>>a>>b>>c;
+cout<<(a*b)/c;
+}
+};
+int main() {
+Begin107 b;
+return 0;
+}
