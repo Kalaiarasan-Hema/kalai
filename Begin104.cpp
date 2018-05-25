b2d18c5c5df1af01469678807270e0077d76abc5
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 11:38:50 +0530

    Create Begin104.cpp

---
+++ Begin104.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class Begin104
+{
+private:
+int n,k;
+public:
+Begin104()
+{
+cin>>n>>k;
+cout<<pow(n,k);
+}
+};
+int main() {
+Begin104 b;
+return 0;
+}
