09e609c34a566f3fa36594d910a55efab8100087
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 11:44:44 +0530

    Create Begin106.cpp

---
+++ Begin106.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Begin106
+{
+private:
+public:
+Begin106()
+{
+int n;
+cin>>n;
+char c=n-1+'A';
+cout<<c;
+}
+};
+int main() {
+Begin106 b;
+return 0;
+}
