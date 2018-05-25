9553d6fa609ea354de5785b7fce6e44557ac09d2
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 10:05:07 +0530

    Create Begin84.cpp

---
+++ Begin84.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+class Begin84
+{
+private:
+int n;
+char ch;
+bool n2b;
+public:
+Begin84()
+{
+cin>>ch;
+n=ch;
+cout<<n;
+}
+};
+int main()
+{
+Begin84 b;
+return 0;
+}
