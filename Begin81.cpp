7d321ff5bf676cb7e97fa95c008787bceb021cd0
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 09:23:33 +0530

    Create Begin81.cpp

---
+++ Begin81.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Begin81
+{
+private:
+int n1,n2,c;
+public:
+void calc()
+{
+while(true)
+{
+cin>>n1>>n2;
+c=(n1>n2)?(n1-n2):(n2-n1);
+cout<<"\n"<<c<<"\n";
+}
+}
+Begin81()
+{
+calc();
+}
+};
+int main()
+{
+Begin81 b;
+return 0;
+}
