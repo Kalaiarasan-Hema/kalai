814666002efcd1e252229e700dea5193c0cadaa3
Kalaiarasan.A <36396214+Kalaiarasan469@users.noreply.github.com>
2018-05-25 11:42:24 +0530

    Create Begin105.cpp

---
+++ Begin105.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+class Begin105
+{
+private:
+public:
+Begin105()
+{
+int n;
+cin>>n;
+char ch=n-1+'A';
+cout<<ch;
+}
+};
+int main() {
+Begin105 b;
+return 0;
+}
